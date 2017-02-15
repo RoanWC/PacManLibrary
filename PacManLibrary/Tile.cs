dffbcd9c21d5cb3a5bb68ede06100a3400419a86
Keylen <Keeson@hotmail.com>
2017-02-15 15:45:41 -0500

    Placed outlining classes into tile

--- PacManLibrary/Tile.cs
+++ PacManLibrary/Tile.cs
@@ -10,5 +10,24 @@ namespace PacManLibrary
 {
     public abstract class Tile
     {
+        public void tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2 Position()
+        {
+            return;
+        }
+
+        public abstract ICollidable Member();
+        public abstract bool CanEnter();
+        public abstract void Collide();
+        public abstract bool IsEmpty();
+        public float GetDistance(Vector2 goal)
+        {
+
+        }
     }
 }
