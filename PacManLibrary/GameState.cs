46a9aad261a340125deb8dbcd98542f8d07159fe
1537595 <1537595@2F14-30>
2017-02-23 17:42:21 -0500

    gamestate stuff

--- PacManLibrary/GameState.cs
+++ PacManLibrary/GameState.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-
+using System.IO;
 
 namespace PacManLibrary
 {
@@ -17,7 +17,20 @@ namespace PacManLibrary
         private Pen pen;
         private ScoreAndLives score;
         public static GameState Parse(string filecontent)
-        {
+        {         
+            string[] read = File.ReadAllLines("levels.txt");
+            GameState gs = new GameState();
+            int[,] grid = new int[23, 23];
+
+            for (int i = 0; i < read.Length; i++)
+            {
+                //look at lab1 specs
+                string[] seperate = read[i].Split('\t');
+                for (int j = 0; j < read.Length; j++)
+                    grid[i, j] = int.Parse(seperate[j]);
+            }
+    
+             //go through grid to get position of every object
             return null;
         }
         public Pacman Pacman
