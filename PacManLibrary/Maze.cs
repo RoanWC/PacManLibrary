96ba6e1e2fd99c51027186fd90e2cfea85dcc440
1010290 <1010290@2F14-07>
2017-03-09 18:36:23 -0500

    Working on maze

--- PacManLibrary/Maze.cs
+++ PacManLibrary/Maze.cs
@@ -14,8 +14,9 @@ namespace PacManLibrary
 
         public Maze()
         {
+            //base(x)
             //Still working on this one
-            maze = new Tile[(int)tilePosition.X, (int)tilePosition.Y];
+            maze = new Maze
         }
         public void SetTiles(Tile[,] tile)
         {
@@ -45,8 +46,24 @@ namespace PacManLibrary
 
         public List<Tile> GetAvailableNeighbours(Vector2 position, Direction direction)
         {
+            List<Tile> availables = new List<Tile>();
 
-            return List;
+            switch (direction)
+            {
+                case Direction.Left:
+                    if(maze((int)position.X, (int)position.Y).Member//ghcfvhgvchj
+                    break;
+
+                case Direction.Right:
+                    break;
+
+                case Direction.Up:
+                    break;
+
+                case Direction.Down:
+                    break;
+            }
+            return availables;
         }
 
         //There has to be a more efficient way to get this done but at the moment this is th eonly
@@ -54,8 +71,7 @@ namespace PacManLibrary
         {
             for( int i = 0; i < maze.GetLength(0); i++)
             {
-                for( int j = 0; j< maze.GetLength(1); j++)
-                {
+                for (int j = 0; j < maze.GetLength(1); j++) {
 
                 }
             }
