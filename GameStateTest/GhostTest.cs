d54ac23d7d27666e1eb30a97e829cb47bea4ed9e
RoanWC <Roanwc@gmail.com>
2017-03-10 16:20:25 -0500

    ch ch ch changes

---
+++ GameStateTest/GhostTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacManLibrary;
+
+namespace GameStateTest
+{
+    [TestClass]
+    public class GhostTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            //arange
+            GameState gs = new GameState();
+            Vector2 target = new Vector2(2, 2);
+            Ghost ghost = new Ghost(gs,5,5,target,
+        }
+    }
+}
