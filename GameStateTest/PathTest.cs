3b60c5eea3bb875990193c29ecdfc89397e12922
Keylen <Keeson@hotmail.com>
2017-03-12 21:55:21 -0400

    Started on Wall test, tile test and path test.

---
+++ GameStateTest/PathTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameStateTest
+{
+    [TestClass]
+    public class PathTest
+    {
+        [TestMethod]
+        public void TestPostion()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCanEnter()
+        {
+        }
+
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+        }
+    }
+}
---
+++ GameStateTest/TileTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacManLibrary;
+
+namespace GameStateTest
+{
+    [TestClass]
+    public class TileTest
+    {
+        [TestMethod]
+        public void TestPostion()
+        {
+            //arrange
+            Wall wall1 = new Wall(1, 1);
+            Wall wall2 = new Wall(1, 1);
+
+            //act
+            //assert
+        }
+
+        [TestMethod]
+        public void TestGetDistance()
+        {
+            //arrange
+            Wall wall1 = new Wall(1, 1);
+            Wall wall2 = new Wall(1, 1);
+            Wall wall3 = new Wall(2, 2);
+
+            //act
+            float distance = wall1.GetDistance(wall2.Position);
+            float diffDistance = wall1.GetDistance(wall3.Position);
+
+            //assert
+            Assert.AreEqual(0.0, distance);
+            Assert.AreNotEqual(distance, diffDistance);
+
+        }
+
+
+    }
+}
