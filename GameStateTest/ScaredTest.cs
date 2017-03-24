32a0a123cbf662085e690b222e4f21b3dff6136d
RoanWC <Roanwc@gmail.com>
2017-03-24 17:38:25 -0400

    push

--- GameStateTest/ScaredTest.cs
+++ GameStateTest/ScaredTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacManLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,34 @@ using System.Threading.Tasks;
 
 namespace GameStateTest
 {
-    class ScaredTest
+    [TestClass]
+    public class ScaredTest
     {
+        [TestMethod]
+        public void testMoveValid()
+        {
+            //arrange
+            GameState gs = new GameState();
+            gs = GameState.Parse("scaredTest.txt");
+            //act
+            //assert
+        }
+        [TestMethod]
+        public void testMoveInvalid()
+        {
+
+        }
     }
 }
+/*
+GameState gs = new GameState();
+gs = GameState.Parse("levels.txt");
+
+            Vector2 target = new Vector2(2, 2);
+Ghost ghost = gs.Ghostpack.Ghosts[1];
+
+//act
+ghost.ChangeState(GhostState.released);
+
+            //assert
+            Assert.IsTrue(ghost.CurrentState is Chase);*/
