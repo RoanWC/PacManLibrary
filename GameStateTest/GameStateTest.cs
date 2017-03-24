413d39cddb4abdfe7ff46e86fc1db8f3e2726cb2
1537595 <1537595@2F14-24>
2017-03-24 16:37:42 -0400

    tests

--- GameStateTest/GameStateTest.cs
+++ GameStateTest/GameStateTest.cs
@@ -20,9 +20,9 @@ namespace GameStateTest
             Tile[,] tile = new Tile[3, 3];
        
             //Wall w = new Wall(8, 0);
-         
+         /*
             Maze m1 = new Maze();
-           
+            // act
             tile[0, 0] = new Wall(0, 0);
             tile[0, 1] = new Wall(0, 1);
             tile[0, 2] = new Wall(0, 2);
@@ -39,16 +39,23 @@ namespace GameStateTest
             tile[2, 2] = new Wall(2, 2);
 
             m1.SetTiles(tile);
-
-
+            */
             Maze m = gs.Maze;
-            Tile w = m[0, 0];
-   
-            Assert.AreSame(m1[0,0].GetType(), w.GetType());
-         
+            Console.WriteLine(m[2, 2].Member());
+
+            // assert
+            Assert.IsTrue(m[0, 0] is Wall);
+            Assert.IsTrue(m[1, 1] is Path);
+            Assert.IsTrue(m[1, 1].Member() is Energizer);
+            Assert.IsTrue(m[2, 2].Member() == null);
+            //Assert.IsTrue(m[2, 3].IsEmpty());
+            // Assert.AreSame(m1[0,0].GetType(), m[0,0].GetType());
+            // Assert.AreSame(m1[1, 1].GetType(), m[1, 1].GetType());
+
+
 
         }
-        /*
+        
         [TestMethod]
         public void TestPellet(){
             //arrange 
@@ -67,7 +74,7 @@ namespace GameStateTest
 
             Assert.AreEqual(points, 100);
         }
-        
+        /*
         [TestMethod]
         public void TestScoreAndLives(){
              GameState gs = new GameState();
